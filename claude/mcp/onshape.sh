4f96527a47b2faceab1185285f2d2cd42dc706f0
Matthew Hyatt <mhyatt000@gmail.com>
2026-04-04 18:32:45 -0500

    onshape

---
+++ claude/mcp/onshape.sh
@@ -0,0 +1,2 @@
+gh repo clone mhyatt000/onshape-mcp
+claude mcp add onshape --env ONSHAPE_ACCESS_KEY=$ONSHAPE_ACCESS_KEY --env ONSHAPE_SECRET_KEY=$ONSHAPE_SECRET_KEY -- uv run --project $HOME/repo/mcp/onshape-mcp onshape-mcp
