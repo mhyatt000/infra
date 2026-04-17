019f5ccc5826737eef4fbeea8a12c3a8de2f00dc
matt <mhyatt@luc.edu>
2026-04-16 19:26:04 -0500

    bazel

---
+++ bazel.sh
@@ -0,0 +1,5 @@
+pixi global install bazel
+
+# mkdir -p ~/.local/bin
+# curl -L https://github.com/bazelbuild/bazelisk/releases/latest/download/bazelisk-linux-amd64 -o ~/.local/bin/bazel
+# chmod +x ~/.local/bin/bazel
