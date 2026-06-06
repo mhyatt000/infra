90f916cb957b3e63f2217fe998874ea6b47794b2
matt <mhyatt@luc.edu>
2026-06-05 23:46:16 -0500

    pixi global tools

--- pixi-uv.sh
+++ pixi-uv.sh
@@ -1,3 +1,4 @@
 curl -fsSL https://pixi.sh/install.sh | sh
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
+pixi global install fd-find bat eza duf zoxide pixi-pack hyperfine rsync croc wget age go-sops
