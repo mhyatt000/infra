6d0333a99545a1100f90288dbca465d5b94a54f2
matt <mhyatt@luc.edu>
2026-06-16 20:33:26 -0400

    pixi ripgrep dust nvim

--- pixi-uv.sh
+++ pixi-uv.sh
@@ -2,3 +2,4 @@ curl -fsSL https://pixi.sh/install.sh | sh
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
 pixi global install fd-find bat eza duf zoxide pixi-pack hyperfine rsync croc wget age go-sops fzf
+pixi global install ripgrep dust nvim # channel exposes nvim not neovim
