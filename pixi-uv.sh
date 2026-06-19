1f5a7528ee924afb6bb6b0c8500184f24fadb15d
matt <mhyatt@luc.edu>
2026-06-19 21:33:38 +0000

    micromamba

--- pixi-uv.sh
+++ pixi-uv.sh
@@ -6,3 +6,4 @@ source ~/.bashrc
 pixi global install fd-find bat eza duf zoxide pixi-pack hyperfine rsync croc wget age go-sops fzf
 pixi global install ripgrep dust btop
 pixi global install nvim # channel exposes nvim not neovim
+pixi global install micromamba
