4fa4dca45c1f4a11edc22f285324798d02132223
matt <mhyatt@luc.edu>
2026-06-19 18:06:13 +0000

    merge

--- pixi-uv.sh
+++ pixi-uv.sh
@@ -1,5 +1,8 @@
 curl -fsSL https://pixi.sh/install.sh | sh
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
+source ~/.bashrc
+
 pixi global install fd-find bat eza duf zoxide pixi-pack hyperfine rsync croc wget age go-sops fzf
-pixi global install ripgrep dust nvim # channel exposes nvim not neovim
+pixi global install ripgrep dust btop
+pixi global install nvim # channel exposes nvim not neovim
